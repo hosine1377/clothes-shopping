73770ec1097c11c4ff7e8671d7cc7871b7b0537d
masome hosine <hosine.masome@gmail.com>
2023-12-25 23:49:01 +0330

    Transfer all states in App component and added provider in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,38 @@ import routes from './routes'
 import { useRoutes } from 'react-router-dom'
 import Header from './Component/Header/Header'
 import Footer from './Component/Footer/Footer'
+import { useState } from 'react'
+import { products, ourProduct } from './data/products'
+import productsContexts from './Contexts/ProductsContexts'
+import Cart from './Component/Cart/Cart'
+import Toast from './Component/Toast/Toast'
 function App() {
+  const [newProducts, setNewProduct] = useState(ourProduct)
+  const [allProducts, setAllProducts] = useState(products)
+  const [userCart, setUserCart] = useState([])
+  const [isShowToast, setIsShowToast] = useState(false)
+  const [isShowCart, setIsShowCart] = useState(false)
   const router = useRoutes(routes)
   return (
     <div>
-      <Header />
-      {router}
-      <Footer />
+      <productsContexts.Provider
+        value={{
+          allProducts,
+          userCart,
+          setUserCart,
+          isShowToast,
+          setIsShowToast,
+          isShowCart,
+          setIsShowCart,
+          newProducts,
+        }}
+      >
+        <Header />
+        {router}
+        <Footer />
+        <Cart />
+        <Toast />
+      </productsContexts.Provider>
     </div>
   )
 }
