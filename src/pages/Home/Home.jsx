1c607e6aaf902ee46cf4241eb073dee0b0843f26
masome hosine <hosine.masome@gmail.com>
2023-12-25 23:51:40 +0330

    using context instead of props in this component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,52 +1,34 @@
-import { useState } from 'react'
+import { useContext } from 'react'
 
 import Banner from '../../Component/Banner/Banner'
 import Brandes from '../../Component/Brandes/Brandes'
 import HomeComponent from '../../Component/Home/Home'
 import New from '../../Component/New/New'
-import { products, ourProduct } from '../../data/products'
 import ProdactsSection from '../../Component/ProdactsSection/ProdactsSection'
 import { Container } from 'react-bootstrap'
-import Toast from '../../Component/Toast/Toast'
-import Cart from '../../Component/Cart/Cart'
 import productsContexts from '../../Contexts/ProductsContexts'
 function Home() {
-  const [newProducts, setNewProduct] = useState(ourProduct)
-  const [allProducts, setAllProducts] = useState(products)
-  const [userCart, setUserCart] = useState([])
-  const [isShowToast, setIsShowToast] = useState(false)
-  const [isShowCart, setIsShowCart] = useState(false)
+  const contextData = useContext(productsContexts)
+  console.log(contextData.newProducts[0].info)
   return (
     <div>
       {/* <main> */}
-      <productsContexts.Provider
-        value={{
-          allProducts,
-          userCart,
-          setUserCart,
-          isShowToast,
-          setIsShowToast,
-          isShowCart,
-          setIsShowCart,
-          newProducts,
-        }}
-      >
-        <HomeComponent />
-        <Brandes />
-        <New />
-        <Container>
-          <ProdactsSection product={newProducts[0]} />
-        </Container>
-        <Banner />
-        <Container>
-          {allProducts.map((product) => (
-            <ProdactsSection key={product.id} product={product} />
-          ))}
-        </Container>
-        {/* </main> */}
-        <Toast />
-        <Cart />
-      </productsContexts.Provider>
+
+      <HomeComponent />
+      <Brandes />
+      <New />
+      <Container>
+        <ProdactsSection product={contextData.newProducts[0]} />
+      </Container>
+      <Banner />
+      <Container>
+        {contextData.allProducts.map((product) => (
+          <ProdactsSection key={product.id} product={product} />
+        ))}
+      </Container>
+      {/* </main> */}
+      {/* <Toast />
+      <Cart /> */}
     </div>
   )
 }
