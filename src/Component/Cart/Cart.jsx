463a5d6983d3fcdd0f6686784c263cd05d477c5e
masome hosine <hosine.masome@gmail.com>
2023-12-25 23:55:11 +0330

    added product in card and show in component

--- src/Component/Cart/Cart.jsx
+++ src/Component/Cart/Cart.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Cart.css'
 import { BsBag } from 'react-icons/bs'
 import { AiOutlineClose } from 'react-icons/ai'
 import { Row, Col } from 'react-bootstrap'
+import productsContexts from '../../Contexts/ProductsContexts'
 function Cart() {
+  const contextData = useContext(productsContexts)
   return (
-    <div className="cart-container ">
+    <div
+      className={`${contextData.isShowCart ? 'active' : ''} cart-container `}
+    >
       <div className="cart-header">
         <BsBag className="bag-icon" />
-        <AiOutlineClose className="close-icon" />
+        <AiOutlineClose
+          className="close-icon"
+          onClick={() => contextData.setIsShowCart(false)}
+        />
       </div>
       <div className="cart-body">
         <div className="cart-body-top">
@@ -28,28 +35,26 @@ function Cart() {
           </Row>
         </div>
         <div className="cart-body-bottom">
-          <Row className="my-2">
-            <Col md="3">
-              <p>clothes</p>
-            </Col>
-            <Col md="3">
-              <p className="d-flex justify-content-between">
-                1 <button className="add-btn">+</button>
-              </p>
-            </Col>
-            <Col md="3">
-              <p>120000</p>
-            </Col>
-            <Col md="3">
-              <div className="img-card">
-                <img
-                  src="../img/featured/4.jpg"
-                  className="img-fluid w-100"
-                  alt=""
-                />
-              </div>
-            </Col>
-          </Row>
+          {contextData.userCart.map((product) => (
+            <Row className="my-2" key={product.id}>
+              <Col md="3">
+                <p>{product.title}</p>
+              </Col>
+              <Col md="3">
+                <p className="d-flex justify-content-between">
+                  {product.count} <button className="add-btn">+</button>
+                </p>
+              </Col>
+              <Col md="3">
+                <p>{product.price}</p>
+              </Col>
+              <Col md="3">
+                <div className="img-card">
+                  <img src={product.img} className="img-fluid w-100" alt="" />
+                </div>
+              </Col>
+            </Row>
+          ))}
         </div>
       </div>
     </div>
