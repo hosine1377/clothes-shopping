5268707f17b5d1fcca8cae22a94ed360e22e3ec2
masome hosine <hosine.masome@gmail.com>
2023-12-25 23:52:56 +0330

    setstate show and hidden

--- src/Component/Toast/Toast.jsx
+++ src/Component/Toast/Toast.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import productsContexts from '../../Contexts/ProductsContexts'
 function Toast() {
+  const contextData = useContext(productsContexts)
   return (
     <div className="toast-container position-fixed bottom-0 me-4 end-0 mb-4">
-      <div className="toast  align-items-center text-white bg-primary">
+      <div
+        className={`${
+          contextData.isShowToast ? 'show' : ''
+        }  toast  align-items-center text-white bg-primary`}
+      >
         <div className="d-flex justify-content-between align-items-center">
-          <buttont typ="button" className="btn-close btnwsite ms-3"></buttont>
+          <buttont
+            typ="button"
+            className="btn-close btnwsite ms-3"
+            onClick={() => {
+              contextData.setIsShowToast(false)
+            }}
+          ></buttont>
           <div className="toast-body">محصول با موفقیت به سبد خرید اضافه شد</div>
         </div>
       </div>
